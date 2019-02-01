9d7ebc72922cf3307a9717b0b0119e88c20b6257
sai-kolla <nagarjunaguf17062@gmail.com>
2019-02-01 17:53:10 -0600

    word count updated
    updated code committed

--- ICP2/source/wordcount.py
+++ ICP2/source/wordcount.py
@@ -1,7 +1,12 @@
 with open('file.txt') as f:
   lines = f.readlines()
-  data = ''.join(lines)
+  data = ''.join(lines[0])
+  data1 = ''.join(lines[1])
   print('lines =',len(lines))
-  print('Words = ',len(data.split()))
+  print('Words in first line = ',len(data.split()))
+  print('Words in second line = ', len(data1.split()))
   char = ''.join(data.split())
-  print('characters = ',len(char))
+  char1 = ''.join(data1.split())
+
+  print('characters in first line= ',len(char))
+  print('characters in second line= ', len(char1))
