ce91297622f908b7e5a2f71e7544b326faf47b45
sai-kolla <nagarjunaguf17062@gmail.com>
2019-04-25 03:41:59 -0500

    icp 10
    icp 10

--- ICP10/Source/1 and 2.py
+++ ICP10/Source/1 and 2.py
@@ -1,5 +1,7 @@
 # Simple CNN model for CIFAR-10
 import numpy
+import keras
+import matplotlib.pyplot as plt
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense
@@ -10,9 +12,8 @@ from keras.optimizers import SGD
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
-from keras import backend as K
-from keras.callbacks import TensorBoard
-K.set_image_dim_ordering('th')
+'''from keras import backend as K
+K.set_image_dim_ordering('th')'''
 
 # fix random seed for reproducibility
 seed = 7
@@ -30,23 +31,21 @@ y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 # Create the model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(64, (3,3),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Conv2D(64, (3,3),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(128, (3,3),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-# flattening the matrix into vector form
+model.add(Conv2D(128, (3,3),activation='relu',padding='same',kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dense(1024,activation='relu',kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
 model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
@@ -58,12 +57,14 @@ decay = lrate/epochs
 sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 print(model.summary())
-tensorboard = TensorBoard(log_dir="logs/{}",histogram_freq=0, write_graph=True, write_images=True)
-model.fit(X_train, y_train, nb_epoch=epochs, batch_size=32,callbacks=[tensorboard])
-# Final evaluation of the model
-
+tbCallBack= keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=10,write_graph=True, write_images=True)
 # Fit the model
-#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32,callbacks=[tbCallBack])
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+#save to disk
+model_json = model.to_json()
+with open('model.json', 'w') as json_file:
+    json_file.write(model_json)
+model.save_weights('model.h5')
