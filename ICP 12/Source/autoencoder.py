4f0c5d930aabc3f2328b8495b4ee29687c194f3c
sai-kolla <nagarjunaguf17062@gmail.com>
2019-05-05 08:52:48 -0500

    icp 11
    icp 11

--- ICP 12/Source/autoencoder.py
+++ ICP 12/Source/autoencoder.py
@@ -1,28 +1,40 @@
-from keras.callbacks import TensorBoard
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.callbacks import TensorBoard
+from keras import regularizers
 
 # this is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+encoding_dim = 32  # 32 floats
 
 # this is our input placeholder
 input_img = Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
+
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(128, activation='relu')(encoded)
+
+# encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(1e-7))(encoded)
+
+# lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(input_img)
 decoded = Dense(784, activation='sigmoid')(encoded)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input_img, decoded)
+
 # this model maps an input to its encoded representation
 encoder = Model(input_img, encoded)
+
 # create a placeholder for an encoded (32-dimensional) input
 encoded_input = Input(shape=(encoding_dim,))
+
 # retrieve the last layer of the autoencoder model
 decoder_layer = autoencoder.layers[-1]
+
 # create the decoder model
 decoder = Model(encoded_input, decoder_layer(encoded_input))
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
 from keras.datasets import mnist
 import numpy as np
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -31,20 +43,17 @@ x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-tensorboard = TensorBoard(log_dir="./1",histogram_freq=0, write_graph=True, write_images=True)
-autoencoder.fit(x_train, x_train,
-                epochs=5,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test),callbacks=[tensorboard])
+tensorboard = TensorBoard(log_dir='1', histogram_freq=0, write_graph=True, write_images=False)
+history=autoencoder.fit(x_train, x_train, epochs=22, batch_size=1024, shuffle=True, validation_data=(x_test, x_test),callbacks=[tensorboard])
+
 # encode and decode some digits
 # note that we take them from the *test* set
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
-# use Matplotlib
+
 import matplotlib.pyplot as plt
-# displaying original and reconstructed image
+
 n = 10  # how many digits we will display
 plt.figure(figsize=(20, 4))
 for i in range(n):
@@ -62,3 +71,21 @@ for i in range(n):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.show()
+
+
+# Saving the model
+model_json = autoencoder.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# serialize weights to HDF5
+autoencoder.save_weights("model.h5")
+
+fig1 = plt.figure()
+plt.plot(history.history['loss'], 'r', linewidth=3.0)
+plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
+plt.xlabel('Epochs ', fontsize=16)
+plt.ylabel('Loss', fontsize=16)
+plt.title('Loss Curves : ', fontsize=16)
+fig1.savefig('loss_lstm.png')
